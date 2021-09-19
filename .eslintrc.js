1cf34a89a640051e9f500ca7b9f45893c2f3a20e
ybw0014 <i@guizhanss.com>
2021-09-20 07:14:20 +0800

    refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {
-    "indent": ["error", 4],
-    "vue/html-indent": "off",
-    "space-before-function-paren": "off",
-    "prefer-const": "off"
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        indent: ['error', 4],
+        'vue/html-indent': 'off',
+        'space-before-function-paren': 'off',
+        'prefer-const': 'off'
+    }
 }
