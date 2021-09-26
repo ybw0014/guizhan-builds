22d672194ba884ae7cc6ee2e911105877784c8f5
ybw0014 <i@guizhanss.com>
2021-09-27 03:37:24 +0800

    chores

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,5 +1,7 @@
 <template>
     <b-navbar type="dark" variant="dark">
-        <b-navbar-brand to="/">ybw0014 的 Maven 构建页面</b-navbar-brand>
+        <b-navbar-brand to="/">
+            ybw0014 的 Maven 构建页面
+        </b-navbar-brand>
     </b-navbar>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
-        "@/assets/styles/app.scss"
+        '@/assets/styles/app.scss'
     ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -35,7 +35,7 @@ export default {
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module',
+        '@nuxtjs/eslint-module'
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
