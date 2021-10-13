881c6d17b41f398f31783f1af8acc448abd88a97
ybw0014 <i@guizhanss.com>
2021-10-13 22:28:18 +0800

    [build skip] fix workflow

--- src/maven-build-setup.sh
+++ src/maven-build-setup.sh
@@ -1,4 +1,4 @@
 git config user.name "GuizhanCraft"
 git config user.email ${GIT_EMAIL}
 
-git remote set-url origin https://${ACCESS_TOKEN}@github.com/ybw0014/maven-builds.git
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/ybw0014/maven-builds.git
