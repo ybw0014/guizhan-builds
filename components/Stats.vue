adbd3a47b925db7806ddadbaf6b5c4796cb0286b
ybw0014 <i@guizhanss.com>
2021-11-10 17:50:28 +0800

    更新统计代码

--- components/Stats.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <!-- Cloudflare Web Analytics -->
-    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "78d27c016d2d41ecb7a0bfc95f59418d"}' />
-    <!-- End Cloudflare Web Analytics -->
-</template>
--- layouts/main.vue
+++ layouts/main.vue
@@ -2,6 +2,9 @@
     <div>
         <page-header />
         <nuxt />
-        <stats />
+
+        <!-- Cloudflare Web Analytics -->
+        <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "78d27c016d2d41ecb7a0bfc95f59418d"}' />
+        <!-- End Cloudflare Web Analytics -->
     </div>
 </template>
