cc9437fd14f77f2df8a832d3757dc2b95b057c96
ybw0014 <i@guizhanss.com>
2021-12-12 11:15:53 +0800

    logger

--- src/maven-build/logger.js
+++ src/maven-build/logger.js
@@ -7,10 +7,28 @@
 
 module.exports = {
     /**
+     * 封装调试输出
+     * @param messages
+     */
+    debug (...messages) {
+        if (process.env.DEBUG === 1) {
+            console.log(messages)
+        }
+    },
+
+    /**
      * 封装日志输出
      * @param messages
      */
     log (...messages) {
         console.log(messages)
+    },
+
+    /**
+     * 封装错误输出
+     * @param messages
+     */
+    error (...messages) {
+        console.error(messages)
     }
 }
