13fc028b7fe36d76af4df05f91da73fd080b6969
ybw0014 <i@guizhanss.com>
2022-01-28 13:52:53 -0500

    remove nuxt storm

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,6 @@ export default {
 
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-        // https://github.com/fumeapp/nuxt-storm
-        'nuxt-storm',
         // https://go.nuxtjs.dev/eslint
         '@nuxtjs/eslint-module',
         // https://go.nuxtjs.dev/tailwindcss
