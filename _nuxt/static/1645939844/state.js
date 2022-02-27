4be56bfdbdb612e65e9f8d3ae1121971fc08d791
GuizhanCraft <no-reply@guizhanss.com>
2022-02-27 15:41:42 +0000

    deploy: dabb9a6dee49d5379d6e36fc81fb180d862ebe35

--- _nuxt/static/1645939844/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645939844",layout:"main",error:a,state:{builds:{latest:{},builds:{}},repos:{repos:a,users:a}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
