eef493cdf5ee0947af76cc4391a3b37e1cadd6dc
ybw0014 <i@guizhanss.com>
2022-06-28 01:50:04 -0400

    [CI skip] Fix

--- src/guizhan-build-setup.sh
+++ src/guizhan-build-setup.sh
@@ -1,4 +1,4 @@
 git config user.name "${BOT_USERNAME}"
 git config user.email "${BOT_EMAIL}"
 
-git remote set-url origin "https://${BOT_TOKEN}@github.com/ybw0014/maven-builds.git"
+git remote set-url origin "https://${BOT_TOKEN}@github.com/ybw0014/guizhan-builds.git"
